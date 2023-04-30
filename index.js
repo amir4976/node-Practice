a09d7d7b361159ea8e2b2b30bec020723ae74978
amir4976 <amirhosianahmadi4976@gmail.com>
2023-04-30 21:58:32 +0430

    node

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const fs = require('fs')
+
+// blocking -- sync
+const textIn = fs.readFileSync('./txt/input.txt')
+console.log(textIn)
+
+const textOut = `this is what we know about the avocado ${textIn} ./n Created on ${Date.now()}`
+fs.writeFileSync('./txt/textOut.txt',textOut)
+
+console.log('file written !')
+
+
+//none blocking -- async
+// fs.readFile('./txt/start.txt','utf-8' ,(err,data)=>{
+//     console.log(data)
+// })
+// console.log('will read file')
